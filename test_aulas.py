5b042fbccc4d9743816b4683c532f93ebce48b50
Andre Pinheiro <apinheiro99@gmail.com>
2022-01-27 21:36:09 -0500

    Teste metodos salva_estado e restaura_contrato

--- test_aulas.py
+++ test_aulas.py
@@ -21,3 +21,25 @@ class Test_aulas(TestCase):
         contrato.avanca()
         print (contrato.tipo)
         self.assertEqual(contrato.tipo, "EM ANDAMENTO")
+
+        historico.adiciona_estado(contrato.salva_estado())
+
+        contrato.avanca()
+        print (contrato.tipo)
+        self.assertEqual(contrato.tipo, "ACERTADO")
+
+        historico.adiciona_estado(contrato.salva_estado())
+
+        contrato.avanca()
+        print (contrato.tipo)
+        self.assertEqual(contrato.tipo, "CONCLUIDO")
+
+        historico.adiciona_estado(contrato.salva_estado())
+
+        contrato.restaura_estado(historico.obtem_estado(1))
+        print (contrato.tipo)
+        self.assertEqual(contrato.tipo, "ACERTADO")
+
+        contrato.restaura_estado(historico.obtem_estado(0))
+        print (contrato.tipo)
+        self.assertEqual(contrato.tipo, "EM ANDAMENTO")
