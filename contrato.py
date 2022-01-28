2e3074260b2710aedc9c355154b139ec6c3022a7
Andre Pinheiro <apinheiro99@gmail.com>
2022-01-27 20:45:26 -0500

    Cherry-pick de aula_02

---
+++ contrato.py
@@ -0,0 +1,77 @@
+from datetime import date
+
+from importlib_metadata import re
+
+class Contrato():
+
+    def __init__(self, data, cliente, tipo):
+        self.__data = data
+        self.__cliente = cliente
+        self.__tipo = tipo
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data (self, data):
+        self.__data = data
+
+    @property
+    def cliente(self):
+        return self.__cliente
+
+    @cliente.setter
+    def cliente(self, cliente):
+        self.__cliente = cliente
+
+    @property
+    def tipo(self):
+        return self.__tipo
+
+    @tipo.setter
+    def tipo(self, tipo):
+        self.__tipo = tipo
+
+    def avanca(self):
+        if self.__tipo == "NOVO":
+            self.__tipo = "EM ANDAMENTO"
+        elif self.__tipo == "EM ANDAMENTO":
+            self.__tipo = "ACERTADO"
+        elif self.__tipo == "ACERTADO":
+            self.__tipo = "CONCLUIDO"
+
+class Estado():
+
+    def __init__(self, contrato):
+        self.__contrato = contrato
+
+    @property
+    def contrato(self):
+        return self.__contrato
+
+class Historico():
+
+    def __init__(self):
+        self.__estados_salvos = []
+
+    def obtem_estado(self, indice):
+        return self.__estados_salvos[indice]
+
+    def adiciona_estado(self, estado):
+        self.__estados_salvos.append(estado)
+
+if __name__ == "__main__":
+
+    historico = Historico()
+
+    contrato = Contrato(
+        data = date.today(),
+        cliente = "Flavio Almeida",
+        tipo = "NOVO"
+    )
+
+    print (contrato.tipo)
+    
+    contrato.avanca()
+    print (contrato.tipo)
